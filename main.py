c43120904f07dc87a4c8ef87cce8918c804c5818
BricoBC <barajas.c.bruno.n@gmail.com>
2023-10-17 15:59:32 -0600

    Modify: Make a functio to module. Make function to every_data is right

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import modulo_information as m_info
 
-def data(category, tema):
-    """
-    Esta función es para obtener los datos totales, los datos con información y los datos sin información.
-    Se necesita enviar como parametro la categoria y el tema.
-    """
-    data_total = df_data['¿Se tiene?'].loc[(df_data['CATEGORIA'] == category) & (df_data['TEMA'] == tema)].size
-    data_without_information = df_data['Campo agregado'].loc[(df_data['CATEGORIA'] == category) & (df_data['TEMA'] == tema) & (df_data['¿Se tiene?'] == 'No')].count()
-    data_information = data_total - data_without_information
-    return data_total, data_without_information, data_information
-
-
 if __name__ == "__main__":
     import pandas as pd
+    import numpy as np
     dashboard = {
         "Categoria": "",
         "Tema": [],
@@ -24,6 +14,10 @@ if __name__ == "__main__":
     df_data_all = pd.read_csv("./KPIs.csv")
         # Elimino las últimas tres columnas
     df_data = df_data_all.iloc[ : , :-3]
+    # Le doy formato a los campos que tienen Si para que sean Sí
+    df_data.loc[df_data['¿Se tiene?'] == 'Si' , '¿Se tiene?'] = 'Sí'
+    df_data.loc[df_data['Campo agregado'] == 'Si' , 'Campo agregado'] = 'Sí'
+
         #Todas las Categorias
     categories = df_data['CATEGORIA'].unique()
     print(categories)
@@ -31,15 +25,36 @@ if __name__ == "__main__":
     category = categories[1]
     arr_datos, arr_without_data, arr_with_data = 0,0,0
 
+    #GET CATEGORIA
     dashboard['Categoria'] = category
-    #theme = temas(category)
+    
+    #GET TEMA
     theme = m_info.filter_column(df_data,'CATEGORIA', category, 'TEMA')
-
     dashboard['Tema'] = theme
+    
+    arr_total_datos = []
+    arr_sin_datos = []
+    arr_con_datos = []
     for j in range(theme.size):
-        dashboard['Datos'].append(data(category, theme[j])[0])
-        dashboard['Datos_sin_informacion'].append(data(category, theme[j])[1])
-        dashboard['Datos_con_informacion'].append(data(category, theme[j])[2])
+        arr_total_datos.append(m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )[0])
+
+        i = m_info.get_i('Sí', m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )[1])
+        # Operación ternaria donde si nos devuelve un True es porque no existe esa palabra por ende el valor es 0, 
+        # en caso de que devuelva la iteral esa la usamos para el array de m_info.every_data
+
+        value = 0 if i == -1 else m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )[2][i]
+        arr_con_datos.append(value)
+
+        i = m_info.get_i('No', m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )[1])
+        value = 0 if i == -1 else m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )[2][i]
+        arr_sin_datos.append(value)
+        print()
+    dashboard['Datos'] = arr_total_datos
+    dashboard['Datos_con_informacion'] = arr_con_datos
+    dashboard['Datos_sin_informacion'] = arr_sin_datos
+
+
+    #GET FUENTE DE INFORMACIÓN
     dashboard['Source_information'] = m_info.filter_column(df_data,'CATEGORIA', category, 'ORIGEN')
 
     print(dashboard)
--- modulo_information.py
+++ modulo_information.py
@@ -1,5 +1,5 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
 #Creo una función que me regresa todos los tema de una categoria
 def filter_column(data, column_of_value, value, column_filter):
@@ -12,3 +12,26 @@ def filter_column(data, column_of_value, value, column_filter):
     """
     arr_tema = data[ data[column_of_value] == value][column_filter].unique()
     return arr_tema
+
+def every_data(data, name_column_category , category, name_column_tema, tema, name_column_values):
+    """
+    Se va a obtener el total de datos, cuántos datos se tiene y cuantos no, esto mediante un filtrado de todos los datos (data) 
+    mediante las columnas de la categoria y del tema en donde se indicará cuál es la categoria y el tema para hacer el filtrado. 
+    Una vez hecho esa filtración sólo se va a obtener la columna indicada en 'name_column_values' para determinar el tamaño de los datos
+    y de estos datos cuántos tienen Sí y cuántos No.
+    """
+    data_total = data[name_column_values].loc[(data[name_column_category] == category) & (data[name_column_tema] == tema)].size
+    data_without_information = data_without_information = data[name_column_values].loc[(data[name_column_category] == category) & 
+                                                                                  (data[name_column_tema] == tema) & 
+                                                                                  (data[name_column_values] == 'No') ].count()
+    data_information = data_total - data_without_information
+    return data_total, data_without_information, data_information
+
+def get_i(word, array):
+    """
+    Con base a un arreglo se buscará una paralabra y retornará la iteral de esa ubicación.
+    """
+    for i in range(len(array)):
+        if(word == array[i]):
+            return i
+    return -1
