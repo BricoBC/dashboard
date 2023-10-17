0bc96559044d8e7e6093a57951032e4370559c97
BricoBC <barajas.c.bruno.n@gmail.com>
2023-10-17 11:50:21 -0600

    Modify: Replace the function local 'source_information' for the function of module

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import modulo_information as m_info
 
-
-def temas(category):
-    """
-    Está función es para obtener todos los temas en forma de un arreglo que contenga la categoria enviada en el parametro.
-    """
-    df_data[df_data['CATEGORIA'] == category]
-    arr_tema = df_data[df_data['CATEGORIA'] == category]['TEMA'].unique()
-    return arr_tema
-
 def data(category, tema):
     """
     Esta función es para obtener los datos totales, los datos con información y los datos sin información.
@@ -18,13 +9,6 @@ def data(category, tema):
     data_without_information = df_data['Campo agregado'].loc[(df_data['CATEGORIA'] == category) & (df_data['TEMA'] == tema) & (df_data['¿Se tiene?'] == 'No')].count()
     data_information = data_total - data_without_information
     return data_total, data_without_information, data_information
-
-def source_information(category):
-    """
-    Esta función es para obtener la fuente de información de todos los temas que tiene la categoria.
-    """
-    source_information = df_data['ORIGEN'].loc[(df_data['CATEGORIA'] == category)].unique()
-    return source_information
 
 
 if __name__ == "__main__":
@@ -48,14 +32,15 @@ if __name__ == "__main__":
     arr_datos, arr_without_data, arr_with_data = 0,0,0
 
     dashboard['Categoria'] = category
-    theme = temas(category)
+    #theme = temas(category)
+    theme = m_info.filter_column(df_data,'CATEGORIA', category, 'TEMA')
 
     dashboard['Tema'] = theme
     for j in range(theme.size):
         dashboard['Datos'].append(data(category, theme[j])[0])
         dashboard['Datos_sin_informacion'].append(data(category, theme[j])[1])
         dashboard['Datos_con_informacion'].append(data(category, theme[j])[2])
-    dashboard['Source_information'] = source_information(category)
+    dashboard['Source_information'] = m_info.filter_column(df_data,'CATEGORIA', category, 'ORIGEN')
 
     print(dashboard)
 
