f0069f7307053df6ba2df754a2f845d1385e281b
BricoBC <barajas.c.bruno.n@gmail.com>
2023-10-18 15:49:40 -0600

    Modify: I hide the iteraction for work with a category

--- main.py
+++ main.py
@@ -15,53 +15,54 @@ if __name__ == "__main__":
     categories = df_data['CATEGORIA'].unique()
     print(categories)
 
-    for i in range(len(categories)):
-        dashboard = {
-            "Categoria": "",
-            "Tema": [],
-            "Datos": [],
-            "Datos_con_informacion": [],
-            "Datos_sin_informacion": [],
-            "Source_information": ""
-        }
+    # for i in range(len(categories)):
+    i=0
+    dashboard = {
+        "Categoria": "",
+        "Tema": [],
+        "Datos": [],
+        "Datos_con_informacion": [],
+        "Datos_sin_informacion": [],
+        "Source_information": ""
+    }
 
-        category = categories[i]
+    category = categories[i]
 
-        #GET CATEGORIA
-        dashboard['Categoria'] = category
+    #GET CATEGORIA
+    dashboard['Categoria'] = category
+    
+    #GET TEMA
+    theme = m_info.filter_column(df_data,'CATEGORIA', category, 'TEMA')
+    dashboard['Tema'] = theme
+    
+    arr_total_datos = []
+    arr_sin_datos = []
+    arr_con_datos = []
+    for j in range(theme.size):
+            # Cómo se tiene el tema de una categoria se empieza a coleccionar en un arreglo los
+            # datos totales, datos con información y datos sin información de cada tema.
+            # Tiene su propio array cada dato obtenido.
+            all_data = m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )
+
+            arr_total_datos.append(all_data[0])
+            arr_con_datos.append(all_data[2])
+            arr_sin_datos.append(all_data[1])
         
-        #GET TEMA
-        theme = m_info.filter_column(df_data,'CATEGORIA', category, 'TEMA')
-        dashboard['Tema'] = theme
-        
-        arr_total_datos = []
-        arr_sin_datos = []
-        arr_con_datos = []
-        for j in range(theme.size):
-                # Cómo se tiene el tema de una categoria se empieza a coleccionar en un arreglo los
-                # datos totales, datos con información y datos sin información de cada tema.
-                # Tiene su propio array cada dato obtenido.
-                all_data = m_info.every_data(df_data, 'CATEGORIA', category, 'TEMA', theme[j], '¿Se tiene?' )
-
-                arr_total_datos.append(all_data[0])
-                arr_con_datos.append(all_data[2])
-                arr_sin_datos.append(all_data[1])
-            
-        dashboard['Datos'] = arr_total_datos
-        dashboard['Datos_con_informacion'] = arr_con_datos
-        dashboard['Datos_sin_informacion'] = arr_sin_datos
+    dashboard['Datos'] = arr_total_datos
+    dashboard['Datos_con_informacion'] = arr_con_datos
+    dashboard['Datos_sin_informacion'] = arr_sin_datos
 
 
-        #GET FUENTE DE INFORMACIÓN
-        dashboard['Source_information'] = m_info.filter_column(df_data,'CATEGORIA', category, 'ORIGEN')
+    #GET FUENTE DE INFORMACIÓN
+    dashboard['Source_information'] = m_info.filter_column(df_data,'CATEGORIA', category, 'ORIGEN')
 
-        print(dashboard)
+    print(dashboard)
 
-        print("Creando las imagenes")
-        fig.bar_pie(dashboard)
-        print(f"Se termino de crear las imagenes de {dashboard['Categoria']}")
-        print(f"Su información es: {dashboard}")
-    print("Fin del proceso")
+    print("Creando las imagenes")
+    fig.bar_pie(dashboard)
+    print(f"Se termino de crear las imagenes de {dashboard['Categoria']}")
+    print(f"Su información es: {dashboard}")
+    #print("Fin del proceso")
 
 
         
